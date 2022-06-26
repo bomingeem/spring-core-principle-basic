657baec531c040297af29af5813fd0e0a29bed75
bmkim <bomingeem@gmail.com>
2022-06-26 22:18:24 +0900

    @Configuration과 싱글톤

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -4,7 +4,6 @@ import hello.core.AppConfig;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberServiceImpl;
 import hello.core.order.OrderServiceImpl;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -21,23 +20,14 @@ public class ConfigurationSingletonTest {
         OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
         MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
 
-        //MemberRepository memberRepository1 = memberService.getMemberRepository();
-//        MemberRepository memberRepository2 = orderService.getMemberRepository();
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
 
-        //System.out.println("memberService -> memberRepository = " + memberRepository1);
-//        System.out.println("orderService -> memberRepository = " + memberRepository2);
+        System.out.println("memberService -> memberRepository = " + memberRepository1);
+        System.out.println("orderService -> memberRepository = " + memberRepository2);
         System.out.println("memberRepository = " + memberRepository);
 
-        //assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
-//        assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
-    }
-
-    @Test
-    void configurationDeep() {
-        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
-        AppConfig bean = ac.getBean(AppConfig.class);
-
-        //스프링이 CGLIB 이라는 바이트코드 조작 라이브러리를 사용해서 AppConfig 클래스를 상속받은 임의의 다른 클래스를 만들고, 그 다른 클래스를 스프링 빈으로 등록한 것
-        System.out.println("bean = " + bean.getClass());
+        assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
     }
 }
