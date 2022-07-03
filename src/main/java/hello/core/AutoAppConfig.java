f4687eaf3f6058919982c4ea1a905e8d06247513
bmkim <bomingeem@gmail.com>
2022-07-03 09:45:28 +0900

    컴포넌트 스캔과 의존관계 자동 주입 시작하기

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,24 +1,12 @@
 package hello.core;
 
-import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        //basePackages = "hello.core.member", -> 탐색할 패키지의 시작 위치를 지정한다. 이 패키지를 포함하여 하위 패키지를 모두 탐색
-        //basePackageClasses = AutoAppConfig.class,
-        //default: @ComponentScan 이 붙은 설정 정보 클래스의 패키지가 시작 위치가 된다
-        //권장하는 방법: 패키지 위치를 지정하지 않고, 설정 정보 클래스의 위치를 프로젝트 최상단에 두는 것
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
-
-//    @Bean(name = "memoryMemberRepository")
-//    MemberRepository memberRepository() {
-//        return new MemoryMemberRepository();
-//    }
 }
