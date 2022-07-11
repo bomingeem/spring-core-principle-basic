388cd8d49a5dbfa54fb9386c47eae773716fd8aa
bmkim <bomingeem@gmail.com>
2022-07-11 21:34:08 +0900

    프로토타입 스코프

--- src/test/java/hello/core/scope/SingletonTest.java
+++ src/test/java/hello/core/scope/SingletonTest.java
@@ -1,6 +1,5 @@
 package hello.core.scope;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -11,7 +10,7 @@ import javax.annotation.PreDestroy;
 import static org.assertj.core.api.Assertions.*;
 
 public class SingletonTest {
-    
+
     @Test
     void singletonBeanFind() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonBean.class);
@@ -20,17 +19,16 @@ public class SingletonTest {
         System.out.println("singletonBean1 = " + singletonBean1);
         System.out.println("singletonBean2 = " + singletonBean2);
         assertThat(singletonBean1).isSameAs(singletonBean2);
-
         ac.close();
     }
-    
+
     @Scope("singleton")
     static class SingletonBean {
         @PostConstruct
         public void init() {
             System.out.println("SingletonBean.init");
         }
-        
+
         @PreDestroy
         public void destroy() {
             System.out.println("SingletonBean.destroy");
